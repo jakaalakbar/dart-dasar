6ad0c3681cc8114204b773dc4a181d471eab3bbe
Jaka Al Akbar <jakaal17@gmail.com>
2023-02-20 15:32:58 +0700

    dart dasar

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  var a = 10;
+  a += 10;
+  print(a);
+
+  var i = 0;
+  // i++;
+  // print(i);
+
+  var j = i++;
+  var k = ++i;
+  print(i);
+  print(j);
+  print(k);
+}
