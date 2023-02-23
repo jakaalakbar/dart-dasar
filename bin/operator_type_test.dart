2929e136dc78fbe4bdf2393bc5de1d7d4401ae2e
Jaka Al Akbar <jakaal17@gmail.com>
2023-02-23 09:45:14 +0700

    dart dasar

---
+++ bin/operator_type_test.dart
@@ -0,0 +1,17 @@
+void main(List<String> args) {
+  dynamic variable = 100;
+
+  // var variableString = variable as String;
+  var variableInt = variable as int;
+
+  print(variable);
+  print(variableInt);
+
+  print(variable is int);
+  print(variable is bool);
+  print(variable is String);
+
+  print(variable is! int);
+  print(variable is! bool);
+  print(variable is! String);
+}
